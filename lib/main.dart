e4ba751386f1bf89812d0a2d4741156cabcb625e
kiwi <bluekiwier@gmail.com>
2026-03-09 17:28:44 +0800

    修复windows平台无法初始化本地数据库问题

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'app.dart';
 import 'services/account_service.dart';
 
 /// 应用入口
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 桌面平台初始化 sqflite_common_ffi
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
 
   // 初始化 AccountService，从本地恢复用户信息
   await AccountService().initialize();
